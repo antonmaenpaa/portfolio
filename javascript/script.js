d9cf625dddaea15e0cefe4ff6b6515e8e3109db4
antonmaenpaa <antonmaenpaa@gmail.com>
2020-11-08 21:24:58 +0100

    add scroll function, change color on header when scroll

--- javascript/script.js
+++ javascript/script.js
@@ -2,15 +2,17 @@ window.addEventListener('load', main);
 
 function main() {
     addEventListeners();
+    scrolling();
 }
 
 function addEventListeners() {
 
     const openMenu = document.getElementById('menu');
     const closeMenu = document.getElementById('close-menu');
-    
+
     openMenu.onclick = openBlock;
     closeMenu.onclick = closeBlock;
+    window.onscroll = scrolling;
 }
 
 function openBlock() {
@@ -22,6 +24,19 @@ function openBlock() {
 function closeBlock() {
     let sideBlock = document.getElementById('sideMenu');
     sideBlock.style.width = '0';
+}
+
+function scrolling() {
+    const headerBlock = document.querySelector('header');
+    let top = window.scrollY;
+    if (top >= 568) {
+        headerBlock.classList.add('header-black');
+        
+    } else {
+        headerBlock.classList.remove('header-black');
+        
+    }
+
 }
 
 
