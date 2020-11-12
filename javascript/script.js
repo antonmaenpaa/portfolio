3391a54881281dd31845b63efe92eb778ed6cc73
antonmaenpaa <antonmaenpaa@gmail.com>
2020-11-12 10:11:23 +0100

    add close sidebar with onclick on links

--- javascript/script.js
+++ javascript/script.js
@@ -8,9 +8,18 @@ function addEventListeners() {
 
     const openMenu = document.getElementById('menu');
     const closeMenu = document.getElementById('close-menu');
+    const sideLinks = document.getElementById('close');
+    const sideLinks1 = document.getElementById('close1');
+    const sideLinks2 = document.getElementById('close2');
+    const sideLinks3 = document.getElementById('close3');
 
+    sideLinks.onclick = closeSideBarWithLinks;
+    sideLinks1.onclick = closeSideBarWithLinks;
+    sideLinks2.onclick = closeSideBarWithLinks;
+    sideLinks3.onclick = closeSideBarWithLinks;
     openMenu.onclick = openSideBar;
     closeMenu.onclick = closeSideBar;
+    
     window.onscroll = changeHeaderBgOnScroll;
 }
 
@@ -21,6 +30,12 @@ function openSideBar() {
 
 
 function closeSideBar() {
+    let sideBlock = document.getElementById('sideMenu');
+    sideBlock.style.width = '0';
+
+}
+
+function closeSideBarWithLinks() {
     let sideBlock = document.getElementById('sideMenu');
     sideBlock.style.width = '0';
 }
